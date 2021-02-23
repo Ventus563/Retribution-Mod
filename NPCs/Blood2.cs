1bb504a2ea4f9eddbf610e8558055be31eb82817
Ventus <sgus2530@gmail.com>
2021-02-23 15:07:24 -0600

    Added Coagulate

---
+++ NPCs/Blood2.cs
@@ -0,0 +1,36 @@
+using Retribution.Dusts;
+using Retribution.Buffs;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Retribution.NPCs
+{
+	public class Blood2 : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Blood");
+		}
+		public override void SetDefaults()
+		{
+			projectile.width = 10;
+			projectile.height = 10;
+			projectile.aiStyle = 14;
+			projectile.friendly = false;
+			projectile.tileCollide = true;
+			projectile.penetrate = -1;
+			aiType = 14;
+
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(BuffID.Bleeding, 180);
+
+		}
+	}
+}
