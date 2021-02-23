9bd99b4157c2c7614462b39d74bd3835a00ac290
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2021-02-23 11:12:05 -0500

    Added Kheru Blood Eater

---
+++ NPCs/KheruBloodeater.cs
@@ -0,0 +1,51 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Retribution.NPCs
+{ 
+	public class KheruBloodeater : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Kheru BloodEater");
+			Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Zombie];
+		}
+
+		public override void SetDefaults()
+		{
+			npc.width = 18;
+			npc.height = 40;
+			npc.damage = 25;
+			npc.defense = 8;
+			npc.lifeMax = 300;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath2;
+			npc.value = 60f;
+			npc.knockBackResist = 0.8f;
+			npc.aiStyle = 3;
+			aiType = NPCID.Zombie;
+			animationType = NPCID.Zombie;
+			banner = Item.NPCtoBanner(NPCID.Zombie);
+			bannerItem = Item.BannerToItem(banner);
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			return SpawnCondition.OverworldNightMonster.Chance * 0.5f;
+		}
+
+		public override void HitEffect(int hitDirection, double damage)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				int dustType = Main.rand.Next(139, 143);
+				int dustIndex = Dust.NewDust(npc.position, npc.width, npc.height, dustType);
+				Dust dust = Main.dust[dustIndex];
+				dust.velocity.X = dust.velocity.X + Main.rand.Next(-50, 51) * 0.01f;
+				dust.velocity.Y = dust.velocity.Y + Main.rand.Next(-50, 51) * 0.01f;
+				dust.scale *= 1f + Main.rand.Next(-30, 31) * 0.01f;
+			}
+		}
+	}
+}
